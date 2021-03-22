9e6fe5ae51777ea697d2e722eaceb18cc35eb69a
Lita Beach <litabeach123@gmail.com>
2021-03-22 11:48:54 -0500

    added models for index, Blog and User

---
+++ models/Blog.js
@@ -0,0 +1,54 @@
+const { Model, DataTypes, Sequelize } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Blog extends Model {
+
+}
+
+Blog.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        description: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id',
+            },
+        },
+
+        createdAt: {
+            type: DataTypes.DATE,
+            defaultValue: Sequelize.fn('now'),
+            allowNull: false
+        },
+
+        updatedAt: {
+            type: DataTypes.DATE,
+            defaultValue: Sequelize.fn('now'),
+            allowNull: false
+        },
+        
+    },
+    {
+        sequelize,
+        timestamps: true,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'blog',
+    }
+);
+
+module.exports = Blog;
