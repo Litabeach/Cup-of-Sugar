3cbe5dac9ba57483027e9d43cb4c327d4b03b427
AP <past0073@umn.edu>
2021-03-24 17:38:41 -0500

    Created get route for gives page.

--- controllers/api/giveRoutes.js
+++ controllers/api/giveRoutes.js
@@ -1,19 +1,84 @@
 const router = require('express').Router();
-const { Post } = require('../../models');
+const { Ask_Give, User } = require('../../models');
+const withAuth = require('../../utils/auth')
 
+// Using the /gives endpoint
 
-//READ all asks by zip code
-router.get('/', async (req, res) => {
-    try {
-        const askData = await Post.findAll({
+//READ all gives
+router.get('/', withAuth, async (req, res) =>{
+    try{
+        const giveData = await Ask_Give.findAll({
             where: {
-                zip_code: req.params.zip_code,
-            }
+                ask_or_give: 'give',
+            },
+            attributes: ['title', 'content', 'resource_type', 'contact', 'createdAt', 'updatedAt'],
+            include: [
+                {
+                    model: User,
+                    attributes: ['name']
+                }
+            ]
+        });
+        const gives = giveData.map((ask_give) => ask_give.get({ plain: true }));
+        res.render('gives', {
+            gives,
+            loggedIn: req.session.logged_in
         })
-        res.status(200).json(askData);
     } catch (err) {
         res.status(500).json(err);
     }
 });
+
+//READ all gives by type of resource
+// router.get('/', withAuth, async (req, res) => {
+//     try {
+//         const giveDAta = await Ask_Give.findAll({
+//             where: {
+//                 ask_or_give: 'give',
+//                 resource_type: req.params.resource_type,
+//             },
+//             attributes: ['title', 'content', 'resource_type', 'contact', 'createdAt', 'updatedAt'],
+//             include: [
+//                 {
+//                     model: User,
+//                     attributes: ['name']
+//                 }
+//             ]
+//         });
+//         const gives = giveData.map((ask_give) => ask_give.get({ plain: true }));
+//         res.render('', {
+//             gives,
+//             loggedIn: req.session.logged_in
+//         })
+//     } catch (err) {
+//         res.status(500).json(err);
+//     }
+// });
+
+//READ all asks by zip code
+// router.get('/', withAuth, async (req, res) => {
+//     try {
+//         const askData = await Post.findAll({
+//             where: {
+//                 ask_or_give: 'give',
+//                 zip_code: req.params.zip_code,
+//             },
+//             attributes: ['title', 'content', 'resource_type', 'contact', 'createdAt', 'updatedAt'],
+//             include: [
+//                 {
+//                     model: User,
+//                     attributes: ['name']
+//                 }
+//             ]
+//         })
+//         const gives = giveData.map((ask_give) => ask_give.get({ plain: true }));
+//         res.render('', {
+//             gives,
+//             loggedIn: req.session.logged_in
+//         })
+//     } catch (err) {
+//         res.status(500).json(err);
+//     }
+// });
 
 module.exports = router;
