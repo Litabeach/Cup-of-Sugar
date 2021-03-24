5cc5c69babe7903e92208414cf4ed50f78b84a67
Anna Fotis <73958785+AnHiFo@users.noreply.github.com>
2021-03-24 11:54:44 -0500

    Helpful category info from tutor

--- public/js/api.js
+++ public/js/api.js
@@ -2,14 +2,14 @@
 const api_key = "75451247a85f3bdd608e2d9ed6f9227d"
 const app_id = "8f3b7de4"
 
-function charityFormHandler(event) { 
+function charityFormHandler(event) {
     event.preventDefault();
     const location = document.querySelector(PLACEHOLDER).value.trim();
     const category = document.querySelector(PLACEHOLDER).value.trim();
     if (location || category) {
-    //This sends the req.query back to the route
-    location.replace(`/organizations?category=${category}&state=${location}`)
-}
+        //This sends the req.query back to the route
+        location.replace(`/organizations?category=${category}&state=${location}`)
+    }
 }
 
 
@@ -25,11 +25,12 @@ router.get('organizations', async (req, res) => {
     console.log(req.query)
     var testUrl = `https://api.data.charitynavigator.org/v2/Organizations?app_id=${process.env.APP_ID}&app_key=${process.env.API_KEY}&state=${req.body.location}`
     try {
-    const response = await axios.get(testUrl);
-    // console.log(data)
-    //need to create view for organizations
+        const response = await axios.get(testUrl);
+        // console.log(data)
+        //need to create view for organizations
         res.render("organizations", {
-        charity: response.data})
+            charity: response.data
+        })
     } catch (err) {
     }
 })
@@ -38,3 +39,9 @@ module.exports = router
 
 
 // Parameters that work for this api: pageSize, search, searchType, rated, fundraisingOrgs, state, city, zip, minRating, maxRating
+
+const categoryBreakdown = [{ category: "Animals", url: "Animals1.jpg", catNum: "1", photoCat: "Animals" }, { category: "Arts Culture Humanities", url: "ArtsCultureHumanities2.jpg", catNum: "2", photoCat: "Arts" },
+{ category: "Education", url: "Education3.jpg", catNum: "3", photoCat: "Education" }, { category: "Environment", url: "Environment4.jpg", catNum: "4", photoCat: "Environment" },
+{ category: "Health", url: "Health5.jpg", catNum: "5", photoCat: "Health" }, { category: "Human Services", url: "HumanServices6.jpg", catNum: "6", photoCat: "Humanity" },
+{ category: "Human Civil Rights", url: "HumanCivilRights8.jpg", catNum: "8", photoCat: "Civil+Rights" }, { category: "Religion", url: "Religion9.jpg", catNum: "9", photoCat: "Religion" },
+{ category: "Research Public Policy", url: "ResearchPublicPolicy11.jpg", catNum: "11", photoCat: "Government" }];
