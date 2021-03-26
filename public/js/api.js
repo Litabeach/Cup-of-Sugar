16bea5ea870b3fcccd211a6465585a48f053fdd5
SageTengu <kyle.kleven@outlook.com>
2021-03-25 19:15:46 -0500

    Updated app_id and api_key variables to exclude actual keys

--- public/js/api.js
+++ public/js/api.js
@@ -1,19 +1,15 @@
-const api_key = "75451247a85f3bdd608e2d9ed6f9227d"
-
-const app_id = "8f3b7de4"
-
 
 function testAPI() { 
 
-var testUrl = "https://api.data.charitynavigator.org/v2/Organizations?app_id=8f3b7de4&app_key=75451247a85f3bdd608e2d9ed6f9227d&state=MN&city=Golden%20Valley&zip=55427"
- $.ajax ({
+var testUrl = "https://api.data.charitynavigator.org/v2/Organizations?"+process.env.app_id+"&"+process.env.api_key+"&state=MN&city=Golden%20Valley&zip=55427";
+$.ajax ({
      method: "GET",
      url: testUrl
  }).then(function (response) {
-     console.log(response)
- })
+     console.log(response);
+ });
 }
 
-testAPI()
+testAPI();
 
 // Parameters that work: pageSize, search, searchType, rated, fundraisingOrgs, state, city, zip, minRating, maxRating
