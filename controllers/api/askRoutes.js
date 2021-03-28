e75af4ccb6e077dad20f05bb7c917b395e57351e
Lita Beach <litabeach123@gmail.com>
2021-03-28 13:59:52 -0500

    works!

--- controllers/api/askRoutes.js
+++ controllers/api/askRoutes.js
@@ -1,48 +1,48 @@
 const router = require('express').Router();
 const { Ask_Give, User, Comment } = require('../../models');
 const withAuth = require('../../utils/auth');
-
 // Using the /api/ask endpoint
 
 //ask Search API
-router.post('/ask_search', withAuth, async (req, res) =>{
-    try{
-        // console.log('Req.body');
-        // console.log(req.body);
+router.get('/ask_search/:category/:askGive/:zip', withAuth, async (req, res) => {
+    try {
+        // console.log(req.params);
         const askData = await Ask_Give.findAll({
-            ...req.body,
             include: [
                 {
                     model: User,
                     attributes: ['name'],
-                  },
-                  {
+                },
+
+                {
                     model: Comment,
                     attributes: ['content', 'createdAt'],
                     include: {
-                      model: User, attributes: ['name'],
+                        model: User, attributes: ['name'],
                     }
-                  },
+                },
             ],
+
             where: {
-                ask_or_give: req.body.askGive,
-                resource_type: req.body.category,
+                ask_or_give: req.params.askGive,
+                resource_type: req.params.category,
                 // city: req.body.city,
-                zip_code: req.body.zip
-                
+                zip_code: req.params.zip
             },
-          
-        
+
         });
-        
+
+
         const asks = askData.map((ask_give) => ask_give.get({ plain: true }));
-        
         console.log('Here is the asks variable: ', asks);
 
         res.render('ask', {
             asks,
             loggedIn: req.session.logged_in
+
         });
+
+
     } catch (err) {
         res.status(500).json(err);
     }
