d9cee3030184802a1b1513b9810da0f0dcf29881
Anna Fotis <73958785+AnHiFo@users.noreply.github.com>
2021-03-28 10:48:44 -0500

    Create Post Successful

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -5,10 +5,31 @@ const { Ask_Give, Comment } = require('../../models');
 //Using the /api/post endpoint
 
 //CREATE new post
+// router.post('/askpost', async (req, res) => {
+//     try {
+//         const postData = await Ask_Give.create(req.body);
+//         res.status(200).json(postData);
+//     } catch (err) {
+//         res.status(500).json(err);
+//     }
+// });
+
 router.post('/askpost', async (req, res) => {
+    console.log("I'M HERE")
     try {
-        const postData = await Ask_Give.create(req.body);
-        res.status(200).json(postData);
+        const newPost = await Ask_Give.create(
+            {
+            ask_or_give: req.body.ask_or_give,
+            title: req.body.title,
+            content: req.body.content,
+            zip_code: req.body.zip_code,
+            resource_type: req.body.resource_type,
+            contact: req.body.contact,
+            user_id: req.session.user_id
+        }
+        );
+        console.log(newPost)
+        res.status(200).json(newPost);
     } catch (err) {
         res.status(500).json(err);
     }
