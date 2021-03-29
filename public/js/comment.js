30bb87368440bce4ae69897f7301c6eeb0a57088
Lita Beach <litabeach123@gmail.com>
2021-03-29 16:28:25 -0500

    comments get and post routes, handlebars and front end javascript all working.

--- public/js/comment.js
+++ public/js/comment.js
@@ -1,84 +1,14 @@
-// const commentFormHandler = async (event) => {
-//     event.preventDefault();
 
-//     const content = document.querySelector('#comment-content').value.trim();
+const newCommentHandler = async function (event) {
+  event.preventDefault();
 
-//     const ask_give_id = window.location.toString().split('/')[
-//         window.location.toString().split('/').length - 1
-//     ];
+  const content = document.querySelector('#comment-content').value.trim();
+  const ask_give_id = document.querySelector('#comment-content').getAttribute("data-id");
+  const user_id = document.querySelector('#commentID').getAttribute("user-id");
 
-//     const user_id = "user"
+  if (content) {
 
-//       const response = await fetch('/api/post/comment/', {
-//         method: 'POST',
-//         body: JSON.stringify({ 
-//             content, ask_give_id, user_id}),
-//         headers: { 'Content-Type': 'application/json' },
-//       });
-
-//       console.log(response)
-//       if (response.ok) {
-//         document.location.reload();
-//       } else {
-//         alert(response.statusText);
-//       }
-//   };
-
-//   document
-//   .querySelector('#commentBtn').addEventListener('click', commentFormHandler);
-
-
-// ////////////////////////////////////////////////////////////////////////////////////////////
-
-// document
-// .querySelector('#commentBtn').addEventListener('click', function (event) {
-//   event.preventDefault();
-
-//   const content = document.querySelector('#comment-content').value.trim();
-
-//   const ask_give_id = window.location.toString().split('/')[
-//       window.location.toString().split('/').length - 1
-//   ];
-
-//   const user_id = "user"
-
-//     const response = fetch('/api/post/comment/', {
-//       method: 'POST',
-//       body: JSON.stringify({ 
-//           content, ask_give_id, user_id}),
-//       headers: { 'Content-Type': 'application/json' },
-//     });
-
-//     console.log(response)
-//     if (response.ok) {
-//       document.location.reload();
-//     } else {
-//       alert(response.statusText);
-//     }
-// });
-
-
-// ////////////////////////////////////////////////////////////////////////////////////////////
-
-
-document
-  .querySelector('#commentBtn')
-  .addEventListener('click', function (event) {
-    event.preventDefault();
-
-    const content = document.querySelector('#comment-content').value.trim();
-    const ask_give_id = document.querySelector('#comment-content').getAttribute("data-id");
-    const user_id = document.querySelector('#commentID').getAttribute("user-id");
-    
-
-
-    console.log(content)
-    console.log(ask_give_id)
-    console.log(user_id)
-
-    // if (content) {
-
-    const response = fetch(`/api/post/comment/`, {
+    const response = await fetch(`/api/post/comment/`, {
       method: 'POST',
       // body: JSON.stringify({ content: content, ask_give_id: ask_give_id }),
       body: JSON.stringify({ content: content, ask_give_id: ask_give_id, user_id: user_id }),
@@ -88,10 +18,13 @@ document
     });
     console.log(response)
 
-    if (response) {
-      document.location.reload;
+    if (response.ok) {
+      document.location.replace(`/api/post/${ask_give_id}`);
     } else {
       alert('Failed to create a new comment');
     }
-    // }
-  });
+  }
+}
+document
+  .querySelector('#commentBtn')
+  .addEventListener('click', newCommentHandler)
