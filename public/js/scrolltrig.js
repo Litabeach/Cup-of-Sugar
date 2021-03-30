b699c810800b3119284d144c1ba057e0858a185e
Omaur B <omaurbliss@gmail.com>
2021-03-30 18:40:55 -0500

    changes to css

--- public/js/scrolltrig.js
+++ public/js/scrolltrig.js
@@ -1,21 +1,45 @@
-gsap.to(".pContent", {
-    yPercent: -100,
-    ease: "none",
+const text = document.querySelector(".hero-title");
+const sub = document.querySelector(".hero-sub");
+
+const tl = gsap.timeline({
+    defaults: { ease: "none", transformOrigin: "50% 50%" },
     scrollTrigger: {
-      trigger: ".pSection",
-      // start: "top bottom", // the default values
-      // end: "bottom top",
+      trigger: ".hero",
+      start: "top top",
+      end: "bottom top",
       scrub: true
-    }, 
+    }
   });
   
-  gsap.to(".pImage", {
-    yPercent: 50,
-    ease: "none",
-    scrollTrigger: {
-      trigger: ".pSection",
-      // start: "top bottom", // the default values
-      // end: "bottom top",
-      scrub: true
-    }, 
+  gsap.utils.toArray("img").forEach((layer) => {
+    const depth = layer.dataset.depth;
+    const movement = -(layer.offsetHeight * depth);
+    tl.to(layer, { y: -movement }, 0);
   });
+  
+  tl.to(
+    text,
+    {
+      y: -text.offsetHeight * text.dataset.depth,
+      autoAlpha: 0,
+      scale: 1.08,
+      duration: 0.2
+    },
+    0
+  )
+    .to(
+      sub,
+      {
+        y: -sub.offsetHeight * sub.dataset.depth,
+        autoAlpha: 0,
+        scale: 1.05,
+        duration: 0.2
+      },
+      0.06
+    )
+    .to(nav, { y: "-100%", duration: 0.16 }, 0)
+    .to(
+      icon,
+      { y: -icon.offsetHeight * icon.dataset.depth, autoAlpha: 0, duration: 0.2 },
+      0
+    );
