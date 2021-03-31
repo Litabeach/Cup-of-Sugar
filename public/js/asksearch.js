458b807b3725e3625aae97738dd40e71463474c6
Lita Beach <litabeach123@gmail.com>
2021-03-31 10:53:13 -0500

    working on the hide for asksearch

--- public/js/asksearch.js
+++ public/js/asksearch.js
@@ -1,43 +1,23 @@
-//search
+  //search
 const askSearchHandler = async (event) => {
   event.preventDefault();
-
+  
   var askGive = document.querySelector('#inputAskGive').value.trim();
   var category = document.querySelector('#inputCategory').value;
   var zip = document.querySelector('#inputZip').value.trim()
 
   var queryURL = `/api/asks/ask_search?askGive=${askGive}&category=${category}&zip=${zip}`
+
   location.replace(queryURL)
+  };
 
-};
-
-// document
-//     .querySelector('#searchAsks').addEventListener('click', askSearchHandler);
-
-
-// function removeHide() {
+//   await function removeHide() {
 //     var noResults = document.getElementById("no-results");
 //     noResults.classList.remove("hide");
 //   }
 
-document
-  .querySelector('#searchAsks')
-  .addEventListener('click', function (event) {
-    //   event.preventDefault();
-    askSearchHandler();
-  });
+//  await removeHide()
 
-    //    function removeHide() {
-    //     var noResults = document.getElementById("no-results");
-    //     noResults.classList.remove("hide");
-    //   }
-
-    // document
-    // .querySelector('#searchAsks')
-    // .addEventListener('click', function(event){
-    //   event.preventDefault();
-    //   removeHide();
-    //   window.stop()
-    //   return;
-    // });
+ document
+ .querySelector('#searchAsks').addEventListener('click', askSearchHandler);
 
