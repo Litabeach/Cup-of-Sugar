8b15b4e2ec8def95d61b2b1af7efee2d4933b0dd
Lita Beach <litabeach123@gmail.com>
2021-03-31 11:53:49 -0500

    working on unhide/search

--- public/js/asksearch.js
+++ public/js/asksearch.js
@@ -1,7 +1,7 @@
-  //search
+//search
 const askSearchHandler = async (event) => {
   event.preventDefault();
-  
+
   var askGive = document.querySelector('#inputAskGive').value.trim();
   var category = document.querySelector('#inputCategory').value;
   var zip = document.querySelector('#inputZip').value.trim()
@@ -9,17 +9,21 @@ const askSearchHandler = async (event) => {
   var queryURL = `/api/asks/ask_search?askGive=${askGive}&category=${category}&zip=${zip}`
 
   location.replace(queryURL)
-  };
+};
 
-//   await function removeHide() {
-//     var noResults = document.getElementById("no-results");
-//     noResults.classList.remove("hide");
-//   }
 
-//  await removeHide()
+document
+  .querySelector('#searchAsks').addEventListener('click', askSearchHandler);
+  
+  
+  function removeHide() {
+    var noResults = document.getElementById("no-results");
+    noResults.classList.remove("hide");
+  }
 
- document
- .querySelector('#searchAsks').addEventListener('click', askSearchHandler);
+
+  document
+  .querySelector('#searchAsks').addEventListener('click', removeHide);
 
 
 
