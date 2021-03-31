b381c3e0ffaaeaf7ac38d0389937afdc1cf4c53b
Omaur B <omaurbliss@gmail.com>
2021-03-31 18:58:21 -0500

    fixed Gsap scroll

--- public/js/scrolltrig.js
+++ public/js/scrolltrig.js
@@ -1,21 +1,67 @@
-gsap.to(".pContent", {
-    yPercent: -100,
-    ease: "none",
-    scrollTrigger: {
-      trigger: ".pSection",
-      // start: "top bottom", // the default values
-      // end: "bottom top",
-      scrub: true
-    }, 
+gsap.set(".content", { autoAlpha: 0 });
+
+var headlines = gsap.utils.toArray(".text");
+
+var totalDuration = 1000;
+var singleDuration = totalDuration / headlines.length;
+
+headlines.forEach((elem, i) => {
+  const smallTimeline = gsap.timeline();
+
+  const content = document.querySelector(".content-wrap");
+  const relevantContent = content.querySelector("span.content-" + i);
+
+  ScrollTrigger.create({
+    trigger: "body",
+
+    start: "top -=" + singleDuration * i,
+    end: "+=" + singleDuration,
+
+    //markers: true,
+
+    animation: smallTimeline,
+    toggleActions: "play reverse play reverse",
   });
-  
-  gsap.to(".pImage", {
-    yPercent: 50,
-    ease: "none",
-    scrollTrigger: {
-      trigger: ".pSection",
-      // start: "top bottom", // the default values
-      // end: "bottom top",
-      scrub: true
-    }, 
-  });
+
+  smallTimeline
+    //.to(elem,{ duration: 0.25, fontSize: "40px", color: "orange"}, 0)
+    .to(elem, { duration: 0.25, color: "orange" }, 0)
+    .set(relevantContent, { autoAlpha: 1 }, 0);
+});
+
+// Everything below this line is not important for your case ---------------------------
+
+const lineTimeline = gsap.timeline();
+
+ScrollTrigger.create({
+  trigger: ".pin-up",
+  start: "top top",
+  end: "+=" + totalDuration,
+  //markers: true,
+  pin: true,
+  scrub: true,
+  animation: lineTimeline,
+});
+
+lineTimeline
+  .to(".sideline", { duration: 1 }, 0)
+  .to(".sideline", { duration: 0.9, scaleY: 1, ease: "none" }, 0);
+
+var showYowza = gsap.timeline();
+showYowza.to(".below span", { autoAlpha: 1, y: 0 });
+
+ScrollTrigger.create({
+  trigger: ".below",
+  start: "top center",
+
+  endTrigger: ".footer",
+  end: "bottom bottom",
+
+  //scrub: 1,
+
+  //markers: true,
+  animation: showYowza,
+
+  toggleActions: "none play none reverse",
+  //toggleActions: "play reverse play reverse"
+});
