92099b6e8c679e06b7b926cb7da7f1fe432068fc
Lita Beach <litabeach123@gmail.com>
2021-03-31 16:05:44 -0500

    search returns "no results found" if no data is returned

--- controllers/api/askRoutes.js
+++ controllers/api/askRoutes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const { Ask_Give, User, Comment } = require('../../models');
+const axios = require('axios');
 
 // Using the /api/ask endpoint
 
@@ -7,14 +8,14 @@ const { Ask_Give, User, Comment } = require('../../models');
 router.get('/ask_search', async (req, res) => {
     try {
         const whereClause = {}
-        if (req.query.askGive){
-            whereClause.ask_or_give=req.query.askGive
+        if (req.query.askGive) {
+            whereClause.ask_or_give = req.query.askGive
         }
-        if (req.query.category){
-            whereClause.resource_type=req.query.category
+        if (req.query.category) {
+            whereClause.resource_type = req.query.category
         }
-        if (req.query.zip){
-            whereClause.zip_code=req.query.zip
+        if (req.query.zip) {
+            whereClause.zip_code = req.query.zip
         }
         const askData = await Ask_Give.findAll({
             include: [
@@ -29,10 +30,18 @@ router.get('/ask_search', async (req, res) => {
 
         const asks = askData.map((ask_give) => ask_give.get({ plain: true }));
 
+        if(asks.length==0){
+            res.render('ask', {
+                loggedIn: req.session.logged_in,
+                message: "No results found."
+            });
+        }
+        else {
         res.render('ask', {
             asks,
             loggedIn: req.session.logged_in
         });
+    }
 
     } catch (err) {
         res.status(500).json(err);
