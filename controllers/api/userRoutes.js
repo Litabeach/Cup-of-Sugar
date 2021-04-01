231b4886caec747c69a69e05ff0ea930d63a28b2
Anna Fotis <73958785+AnHiFo@users.noreply.github.com>
2021-03-31 22:53:17 -0500

    Login Troubleshoot Attempt

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -45,7 +45,7 @@ router.post('/login', async (req, res) => {
         req.session.user_id = userData.id;
         req.session.name = userData.name;
         req.session.logged_in = true;
-        res.json({ user: userData, message: 'You are now logged in!' });
+        // res.json({ user: userData, message: 'You are now logged in!' });
         //need res.render navigation page when someone logs in
       });
       res.render('navigation', {
