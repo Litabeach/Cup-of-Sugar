5bd85360901601bef9ee5ba0be39d4a0f7ba70b5
Lita Beach <litabeach123@gmail.com>
2021-03-31 23:20:04 -0500

    set session cookie maxage to six hours

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const hbs = exphbs.create({ helpers });
 const sess = {
   secret: 'Super secret secret',
   cookie: {
-    // maxAge: 10
+    maxAge: 21600000
   },
   resave: false,
   saveUninitialized: true,
