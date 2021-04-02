107df353f95867153ea9137c39fff7a06fcd2e92
Lita Beach <litabeach123@gmail.com>
2021-04-01 20:59:01 -0500

    cleaned up comments in userRoutes

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -12,7 +12,6 @@ router.post('/signup', async (req, res) => {
         req.session.name = userData.name;
         req.session.logged_in = true;
         res.status(200).json(userData);
-        //need res.render navigation page when someone registers
       });
     } catch (err) {
       res.status(400).json(err);
@@ -45,29 +44,21 @@ router.post('/login', async (req, res) => {
         req.session.name = userData.name;
         req.session.logged_in = true;
         res.json({ user: userData, message: 'You are now logged in!' });
-        //need res.render navigation page when someone logs in
       });
-    //   res.render('navigation', {
-    //     loggedIn: req.session.logged_in
-    // });
     } catch (err) {
       res.status(400).json(err);
     }
   });
 
   //Logout
-  // redirect user to the landing page
   router.post('/logout', (req, res) => {
     console.log("Beginning of logout route");
     if (req.session.logged_in) {
       console.log('We are deleting the session');
       req.session.destroy(() => {
         res.status(204).end();
-        
-        //present a snackbar/alert stating "You have logged out"
       });
       console.log('You are now logged out!');
-      //res.render('navigation');
     } else {
       res.status(404).end();
     }
