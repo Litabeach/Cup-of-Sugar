f06c72abf9401cd9431a7e33eb068070738bd549
Anna Fotis <73958785+AnHiFo@users.noreply.github.com>
2021-04-01 21:48:25 -0500

    JS commented

--- public/js/asksearch.js
+++ public/js/asksearch.js
@@ -1,16 +1,18 @@
-//search
+//function to search for asks
 const askSearchHandler = async (event) => {
     event.preventDefault();
     
+    // variables
     var askGive = document.querySelector('#inputAskGive').value.trim();
     var category = document.querySelector('#inputCategory').value;
     var zip = document.querySelector('#inputZip').value.trim()
     
-
+    // url
     var queryURL = `/api/asks/ask_search?askGive=${askGive}&category=${category}&zip=${zip}`
 
     location.replace(queryURL)
     };
   
+    //on click event
    document
    .querySelector('#searchAsks').addEventListener('click', askSearchHandler);
