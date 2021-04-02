5add9eb7728cae80bc69b644efe492541746a64f
SageTengu <kyle.kleven@outlook.com>
2021-04-01 23:32:56 -0500

    Adjusted container width to better fit each page, aligned labels to the left, reformatted create post form, and moved create post buttons above post lists, and some other small tweaks

--- server.js
+++ server.js
@@ -4,12 +4,11 @@ const session = require('express-session');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
 const helpers = require('./utils/helpers');
-
 const sequelize = require('./config/connection');
-
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
+
 const PORT = process.env.PORT || 3001;
 
 const hbs = exphbs.create({ helpers });
